9325bb344c91a4aaa7176379210e6788ca50d58e
David Lambertson <dlamber3@mail.umw.edu>
2020-06-16 03:06:43 -0400

    The code the play is done, all that is left is one more design pattern.  See my last commit to read how to run the game with sockets!

--- project/src/project/old/BlackJackSim.java
+++ project/src/project/old/BlackJackSim.java
@@ -49,10 +49,14 @@ public class BlackJackSim {
 		String[] deck = createNewDeck(shoeSize);
 		String[] playedCards = new String[shoeSize];
 
+		//players' names
+		String p1Name = player1.getPlayerName();
+		String p2Name = player2.getPlayerName();
+
 		// place bets
 		int bet1 = player1.placeBet(player1.getBank(), playedCards, numCardsLeft);
-		System.out.println("Player Bank = " + player1.getBank());
-		System.out.println("Player bets " + bet1 + "\n==========");
+		System.out.println(p1Name + " Bank = " + player1.getBank());
+		System.out.println(p1Name + " bets " + bet1 + "\n==========");
 
 		//deal initial cards
 		String[] playerCards = createEmptyHand(5);
@@ -79,10 +83,10 @@ public class BlackJackSim {
 				nextShoeIndex++;
 				numCardsLeft--;
 				nextPlayerHandIndex++;
-				System.out.println("Player hits.");
+				System.out.println(p1Name + " hits.");
 			}
 			else
-				System.out.println("Player stands.");
+				System.out.println(p1Name + " stands.");
 		}
 
 		// dealer's hand
@@ -104,13 +108,13 @@ public class BlackJackSim {
 		int dealerPoints = BlackjackRules.countPoints(dealerCards);
 		int playerPoints = BlackjackRules.countPoints(playerCards);
 		System.out.println("==========\nDealer has: " + dealerPoints);
-		System.out.println("Player has: " + playerPoints);
+		System.out.println(p1Name + " has: " + playerPoints);
 
 		// figure out winner - this is not correct for all cases, but it is close enough to test with
 		boolean wonHand;
 		if (dealerPoints > playerPoints && dealerPoints < 22
 				|| playerPoints > 21) {
-			System.out.println("Player lost!");
+			System.out.println(p1Name + " lost!");
 			wonHand = false;
 			player1.modifyBank(wonHand, bet1);
 		} else if (dealerPoints == 21) {// check dealer blackjack
@@ -126,7 +130,7 @@ public class BlackJackSim {
 				System.out.println("Push game!");
 			}
 		} else if(playerPoints == 21 && playerCards[2]=="0") { // check player blackjack
-			System.out.println("Player has Blackjack!!!");
+			System.out.println(p1Name + " has Blackjack!!!");
 			player1.modifyBank(true, (int) 1.5*bet1);
 		} else if (playerPoints == dealerPoints){
 			System.out.println("Push game!");
@@ -135,6 +139,108 @@ public class BlackJackSim {
 			player1.modifyBank(true, bet1);
 		}
 		System.out.println("Player bank = " + player1.getBank());
+		System.out.println("\n" + p2Name + "'s turn!!!\n");
+
+		/// BEGINS PLAYER 2's Turn /////////////
+
+		// generate a random list of cards for a sample deck of 20
+		shoeSize = 20;
+		numCardsLeft = shoeSize;
+		deck = createNewDeck(shoeSize);
+		playedCards = new String[shoeSize];
+
+		//players' names
+		p1Name = player1.getPlayerName();
+		p2Name = player2.getPlayerName();
+
+		// place bets
+		int bet2 = player2.placeBet(player1.getBank(), playedCards, numCardsLeft);
+		System.out.println(p2Name + " Bank = " + player2.getBank());
+		System.out.println(p2Name + " bets " + bet2 + "\n==========");
+
+		//deal initial cards
+		playerCards = createEmptyHand(5);
+		dealerCards = createEmptyHand(5);
+		dealerUpCard = "";
+		nextShoeIndex = 0;
+		nextPlayerHandIndex = 2;
+		nextDealerHandIndex = 2;
+		playerCards[0] = deck[0];
+		dealerCards[0] = deck[1];
+		playerCards[1] = deck[2];
+		dealerCards[1] = deck[3];
+		numCardsLeft -= 4;
+		dealerUpCard = dealerCards[1];
+		nextShoeIndex = 4;
+
+		// player's hand
+		doesPlayerHit = true;
+		while (doesPlayerHit) {
+			printCurrentHandInfo(playerCards);
+			doesPlayerHit = player2.doesPlayerHit(playerCards, dealerUpCard);
+			if (doesPlayerHit) {
+				playerCards[nextPlayerHandIndex] = deck[nextShoeIndex];
+				nextShoeIndex++;
+				numCardsLeft--;
+				nextPlayerHandIndex++;
+				System.out.println(p2Name + " hits.");
+			}
+			else
+				System.out.println(p2Name + " stands.");
+		}
+
+		// dealer's hand
+		doesDealerHit = true;
+		while (doesDealerHit) {
+			printCurrentHandInfo(dealerCards);
+			doesDealerHit = BlackjackRules.doesDealerHit(dealerCards);
+			if (doesDealerHit) {
+				dealerCards[nextDealerHandIndex] = deck[nextShoeIndex];
+				nextShoeIndex++;
+				numCardsLeft--;
+				nextDealerHandIndex++;
+				System.out.println("Dealer hits.");
+			}
+			else
+				System.out.println("Dealer stands.");
+		}
+
+		dealerPoints = BlackjackRules.countPoints(dealerCards);
+		playerPoints = BlackjackRules.countPoints(playerCards);
+		System.out.println("==========\nDealer has: " + dealerPoints);
+		System.out.println(p2Name + " has: " + playerPoints);
+
+		// figure out winner - this is not correct for all cases, but it is close enough to test with
+		//boolean wonHand;
+		if (dealerPoints > playerPoints && dealerPoints < 22
+				|| playerPoints > 21) {
+			System.out.println(p2Name + " lost!");
+			wonHand = false;
+			player2.modifyBank(wonHand, bet1);
+		} else if (dealerPoints == 21) {// check dealer blackjack
+			if (dealerCards[2]=="0") {
+				if (playerPoints == 21 && playerCards[2]=="0") {
+					System.out.println("Push game!");
+				}
+				else {
+					System.out.println("Dealer has Blackjack!!!");
+					player2.modifyBank(false, bet1);
+				}
+			} else if (playerPoints == dealerPoints){
+				System.out.println("Push game!");
+			}
+		} else if(playerPoints == 21 && playerCards[2]=="0") { // check player blackjack
+			System.out.println(p2Name + " has Blackjack!!!");
+			player2.modifyBank(true, (int) 1.5*bet1);
+		} else if (playerPoints == dealerPoints){
+			System.out.println("Push game!");
+		} else {
+			System.out.println("Player won!");
+			player2.modifyBank(true, bet1);
+		}
+		System.out.println(p2Name + "'s bank = " + player2.getBank());
+
+		/// END PLAYER 2's turn ///////////////////////
 
         System.out.println("Play again? [Y/N]");
         String n = userIn.next();
