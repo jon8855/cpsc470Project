e54ae5c7474bc52500a4afdef16b39aa91130647
Brandon Frulla <bfrulla@mail.umw.edu>
2020-06-16 11:16:06 -0500

    Implemented the last design pattern and will have another commit coming soon with cleaner, more presentable code for final submission. @Dave remember to comment the packages out again, sorry bud

---
+++ project/src/project/old/PlayerMindState.java
@@ -0,0 +1,9 @@
+package project.old;
+
+//this interface influences how the player is feeling, and therefore how they place their bets
+
+public interface PlayerMindState {
+
+	public boolean isFeelingLucky();
+
+}
